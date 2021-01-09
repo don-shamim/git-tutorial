d6afe825cbf341f41461956d7dde002fb7a0bc90
don-shamim <nsu.shamim@gmail.com>
2021-01-10 00:18:45 +0600

    Added a new method to display to hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
 	public static void public static void main(String[] args) {
 		System.out.println("Vs code is awesome");
+
+		greeting("Shamim Ahmed");
+	}
+
+	public static void greeting(String name){
+		System.out.println("Wellcome "+name);
 	}
 }
