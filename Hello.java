cc8d5270085727e0bb2447ae68be2cadc8338a37
don-shamim <nsu.shamim@gmail.com>
2021-01-10 01:13:17 +0600

    All method remove and add message method

--- Hello.java
+++ Hello.java
@@ -2,18 +2,12 @@ public class Hello {
 	public static void public static void main(String[] args) {
 		System.out.println("Vs code is awesome");
 
-		display();
+		
 	}
 
-	public static greeting(String name){
-		System.out.println("Wellcome "+name);
+	public static void message(){
+		System.out.println("Hurreh!!! Now we are able to contribute open source");
+		
 	}
-
-	public static void display(){
-		System.out.println("Hello Hello");
-	}
-
-	public static sqr(int x){
-		return x*x;
-	}
+	
 }
