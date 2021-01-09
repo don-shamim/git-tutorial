202539dd849912760c0bdd9ce44f3313d96369c0
don-shamim <nsu.shamim@gmail.com>
2021-01-10 00:34:50 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello {
 	public static void public static void main(String[] args) {
 		System.out.println("Vs code is awesome");
 
-		greeting("Shamim Ahmed");
+		greeting("git");
 	}
 
 	public static void greeting(String name){
-		System.out.println("Wellcome "+name);
+		System.out.println("Bye Bye "+name);
 	}
 }
